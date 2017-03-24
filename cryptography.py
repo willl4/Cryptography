e1227a1d1df882e5799f3b8c2d758794c7aa118c
willl4 <williamlaycock@hanovernorwichschools.org>
2017-03-24 14:59:31 -0400

    Updated from Brython Server: March 24, 2017 2:59:34 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -13,55 +13,56 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 
 t=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 o=0
-if t is 'e':
-    m=input("message: ")
-    numbers=[]
-    for c in m:
-        n=associations.find(c)
-        numbers.append(n)
-    k=input("key: ")
-    letters=[]
-    for c in k:
-        l=associations.find(c)
-        letters.append(l)
-    s=len(letters)
-    p=len(numbers)
-    while s < p:
-        letters=letters*2
+while o==0:
+    if t is 'e':
+        m=input("message: ")
+        numbers=[]
+        for c in m:
+            n=associations.find(c)
+            numbers.append(n)
+        k=input("key: ")
+        letters=[]
+        for c in k:
+            l=associations.find(c)
+            letters.append(l)
         s=len(letters)
-    q=list(zip(numbers,letters))
-    result=[]
-    for a,b in q:
-        result.append(a+b)
-    result=[associations[x] for x in result]
-    print("".join(result))
-    
-    
-elif t is 'd':
-    m=input("message: ")
-    numbers=[]
-    for c in m:
-        n=associations.find(c)
-        numbers.append(n)
-    k=input("key: ")
-    letters=[]
-    for c in k:
-        l=associations.find(c)
-        letters.append(l)
-    s=len(letters)
-    p=len(numbers)
-    while s < p:
-        letters=letters*2
+        p=len(numbers)
+        while s < p:
+            letters=letters*2
+            s=len(letters)
+        q=list(zip(numbers,letters))
+        result=[]
+        for a,b in q:
+            result.append(a+b)
+        result=[associations[x] for x in result]
+        print("".join(result))
+        
+        
+    elif t is 'd':
+        m=input("message: ")
+        numbers=[]
+        for c in m:
+            n=associations.find(c)
+            numbers.append(n)
+        k=input("key: ")
+        letters=[]
+        for c in k:
+            l=associations.find(c)
+            letters.append(l)
         s=len(letters)
-    q=list(zip(numbers,letters))
-    result=[]
-    for a,b in q:
-        result.append(a-b)
-    result=[associations[x] for x in result]
-    print("".join(result))
-    
-elif t is 'q':
-    print("Goodbye!")
-    o=1
-else:
-    print("Did not understand command, try again.")
+        p=len(numbers)
+        while s < p:
+            letters=letters*2
+            s=len(letters)
+        q=list(zip(numbers,letters))
+        result=[]
+        for a,b in q:
+            result.append(a-b)
+        result=[associations[x] for x in result]
+        print("".join(result))
+        
+    elif t is 'q':
+        print("Goodbye!")
+        o=1
+    else:
+        print("Did not understand command, try again.")
